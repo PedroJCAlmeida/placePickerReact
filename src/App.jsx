40b64d28b4f9901c21288009ef7a55b1212765d0
PedroJCAlmeida <pedrojoaocarvalhoalmeida@gmail.com>
2025-02-07 11:58:29 +0000

    useEffect cleanup function and progressbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 
 import logoImg from './assets/logo.png';
 import DeleteConfirmation from './components/DeleteConfirmation.jsx';
@@ -61,7 +61,7 @@ function App() {
     }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
@@ -72,7 +72,9 @@ function App() {
       "selectedPlaces", 
       JSON.stringify(storedIds.filter((id) => id !== selectedPlace.current))
   );
-  }
+  }, []);
+
+  
 
   return (
     <>
